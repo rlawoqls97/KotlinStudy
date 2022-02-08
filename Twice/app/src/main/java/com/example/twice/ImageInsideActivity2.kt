46597ee1f840eb515d955cf22180ed17c46d62f7
김재빈 <jaebin@gimjaebin-ui-MacBookPro.local>
2022-02-08 16:22:05 +0900

    Kotlin study

---
+++ Twice/app/src/main/java/com/example/twice/ImageInsideActivity2.kt
@@ -0,0 +1,32 @@
+package com.example.twice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
+
+class ImageInsideActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_image_inside2)
+
+        val getData = intent.getStringExtra("data")
+
+        val memberImage = findViewById<ImageView>(R.id.memberImageArea)
+//        Toast.makeText(this, getData, Toast.LENGTH_LONG).show()
+        when(getData){
+            "1" -> memberImage.setImageResource(R.drawable.member_1)
+            "2" -> memberImage.setImageResource(R.drawable.member_2)
+            "3" -> memberImage.setImageResource(R.drawable.member_3)
+            "4" -> memberImage.setImageResource(R.drawable.member_4)
+            "5" -> memberImage.setImageResource(R.drawable.member_5)
+            "6" -> memberImage.setImageResource(R.drawable.member_6)
+            "7" -> memberImage.setImageResource(R.drawable.member_7)
+            "8" -> memberImage.setImageResource(R.drawable.member_8)
+            "9" -> memberImage.setImageResource(R.drawable.member_9)
+
+        }
+    }
+}
